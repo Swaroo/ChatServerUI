647f150de18c07036faabae88c6e1da431a74963
Swaroo <swaroopsrinivasa@gmail.com>
2019-10-29 10:31:50 +0000

    Updates

---
+++ precache-manifest.e5bed48f5d0211f5133f3dd1279dd020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81d98472917fccb989d7ec082320acf",
+    "url": "/ChatServerUI/index.html"
+  },
+  {
+    "revision": "fb4fd07ca41f52a716b5",
+    "url": "/ChatServerUI/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e60d9bf910c4f71a1cdf",
+    "url": "/ChatServerUI/static/js/2.0e2d7486.chunk.js"
+  },
+  {
+    "revision": "fb4fd07ca41f52a716b5",
+    "url": "/ChatServerUI/static/js/main.d133e183.chunk.js"
+  },
+  {
+    "revision": "4a9e49915882c02a020e",
+    "url": "/ChatServerUI/static/js/runtime-main.18fb9a9d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ChatServerUI/static/media/logo.25bf045c.svg"
+  }
+]);
