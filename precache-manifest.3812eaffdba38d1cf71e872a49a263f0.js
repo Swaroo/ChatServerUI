acece0c9d0a203ef7ca0ae5a88cf133d6ea61ba5
Swaroo <swaroopsrinivasa@gmail.com>
2019-10-29 10:40:08 +0000

    Updates

---
+++ precache-manifest.3812eaffdba38d1cf71e872a49a263f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666cf194b7617a18b892d2d642424a51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4fd07ca41f52a716b5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e60d9bf910c4f71a1cdf",
+    "url": "/static/js/2.0e2d7486.chunk.js"
+  },
+  {
+    "revision": "fb4fd07ca41f52a716b5",
+    "url": "/static/js/main.d133e183.chunk.js"
+  },
+  {
+    "revision": "82c1bdd4bb076f74e2c1",
+    "url": "/static/js/runtime-main.2e988a3d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
